2c6baf30822c53063a8bddaeea3bff6136a7740b
nicobell <nico.bellucco@gmail.com>
2023-11-29 15:08:46 +0100

    first dist

--- src/store/index.js
+++ src/store/index.js
@@ -152,7 +152,325 @@ export default new Vuex.Store({
         "Western Sahara": 0,
         "Zambia": 6,
         "Zimbabwe": 23
-      }
+      },
+      "institutes": [
+        {
+          "name": "Rhodes University",
+          "city": "Grahamstown",
+          "country": "South Africa",
+          "latitude": -33.3042,
+          "longitude": 26.5328
+        },
+        {
+          "name": "University of Nairobi",
+          "city": "Nairobi",
+          "country": "Kenya",
+          "latitude": -1.286389,
+          "longitude": 36.817223
+        },
+        {
+          "name": "University of Pretoria",
+          "city": "Pretoria",
+          "country": "South Africa",
+          "latitude": -25.7449,
+          "longitude": 28.1878
+        },
+        {
+          "name": "University of Port Elizabeth",
+          "city": "Port Elizabeth",
+          "country": "South Africa",
+          "latitude": -33.917,
+          "longitude": 25.5701
+        },
+        {
+          "name": "University of Johannesburg",
+          "city": "Johannesburg",
+          "country": "South Africa",
+          "latitude": -26.1833674,
+          "longitude": 25.4884087
+        },
+        {
+          "name": "University of Botswana",
+          "city": "Gaborone",
+          "country": "Botswana",
+          "latitude": -24.653257,
+          "longitude": 25.906792
+        },
+        {
+          "name": "University of Cape Town",
+          "city": "Cape Town",
+          "country": "South Africa",
+          "latitude": -33.9249,
+          "longitude": 18.4241
+        },
+        {
+          "name": "University of the Free State",
+          "city": "Bloemfontein",
+          "country": "South Africa",
+          "latitude": -29.0852,
+          "longitude": 26.1596
+        },
+        {
+          "name": "Universit\u00e9 Cheikh Anta Diop de Dakar",
+          "city": "Dakar",
+          "country": "Senegal",
+          "latitude": 14.6937,
+          "longitude": -17.4441
+        },
+        {
+          "name": "University of Cape Town",
+          "city": "Cape Town",
+          "country": "South Africa",
+          "latitude": -33.9249,
+          "longitude": 18.4241
+        },
+        {
+          "name": "University of the Free State",
+          "city": "Bloemfontein",
+          "country": "South Africa",
+          "latitude": -29.087217,
+          "longitude": 26.154898
+        },
+        {
+          "name": "Université d’Abomey Calavi (UAC)",
+          "city": "Abomey-Calavi",
+          "country": "Benin",
+          "latitude": 6.441,
+          "longitude": 2.352
+        },
+        {
+          "name": "Université Nationale du Benin",
+          "city": "Porto-Novo",
+          "country": "Benin",
+          "latitude": 6.44105,
+          "longitude": 2.35234
+        },
+        {
+          "name": "University of Johannesburg",
+          "city": "Johannesburg",
+          "country": "South Africa",
+          "latitude": -26.1833,
+          "longitude": 27.9927
+        },
+        {
+          "name": "University of Natal",
+          "city": "Durban",
+          "country": "South Africa",
+          "latitude": 29.868038,
+          "longitude": 30.980501
+        },
+        {
+          "name": "Medical University of South Africa (MEDUNSA)",
+          "city": "Ga-Rankuwa",
+          "country": "South Africa",
+          "latitude": -25.62,
+          "longitude": 27.98
+        },
+        {
+          "name": "International Centre for Research in Agroforestry (ICRAF)",
+          "city": "Nairobi",
+          "country": "Kenya",
+          "latitude": -1.286389,
+          "longitude": 36.817223
+        },
+        {
+          "name": "University of Ibadan",
+          "city": "Ibadan",
+          "country": "Nigeria",
+          "latitude": 7.3912,
+          "longitude": 3.9167
+        },
+        {
+          "name": "Malaria Research & Training Center",
+          "city": "Bamako",
+          "country": "Mali",
+          "latitude": 12.6392,
+          "longitude": -8.0029
+        },
+        {
+          "name": "University of Stellenbosch",
+          "city": "Stellenbosch",
+          "country": "South Africa",
+          "latitude": -33.9321,
+          "longitude": 18.8602
+        },
+        {
+          "name": "University of KwaZulu-Natal",
+          "city": "Durban",
+          "country": "South Africa",
+          "latitude": -29.8579,
+          "longitude": 31.0292
+        },
+        {
+          "name": "Kenya Medical Research Institute (KEMRI)",
+          "city": "Nairobi",
+          "country": "Kenya",
+          "latitude": -1.286389,
+          "longitude": 36.817223
+        },
+        {
+          "name": "University of Cape Town (UCT)",
+          "city": "Cape Town",
+          "country": "South Africa",
+          "latitude": -33.9249,
+          "longitude": 18.4241
+        },
+        {
+          "name": "International Institute of Tropical Agriculture (IITA)",
+          "city": "Ibadan",
+          "country": "Nigeria",
+          "latitude": 7.3912,
+          "longitude": 3.9167
+        },
+        {
+          "name": "International Livestock Research Institute (ILRI)",
+          "city": "Nairobi",
+          "country": "Kenya",
+          "latitude": -1.286389,
+          "longitude": 36.817223
+        },
+        {
+          "name": "University of the Western Cape",
+          "city": "Cape Town",
+          "country": "South Africa",
+          "latitude": -33.9249,
+          "longitude": 18.4241
+        },
+        {
+          "name": "Université de Lomé",
+          "city": "Lomé",
+          "country": "Togo",
+          "latitude": 6.1725,
+          "longitude": 1.2314
+        },
+        {
+          "name": "Addis Ababa University",
+          "city": "Addis Ababa",
+          "country": "Ethiopia",
+          "latitude": 9.005401,
+          "longitude": 38.763611
+        },
+        {
+          "name": "Université Cheikh Anta Diop de Dakar",
+          "city": "Dakar",
+          "country": "Senegal",
+          "latitude": 14.6937,
+          "longitude": -17.4441
+        },
+        {
+          "name": "DST/NRF Centre of Excellence in Epidemiological Modelling and Analysis",
+          "city": "Stellenbosch",
+          "country": "South Africa",
+          "latitude": -33.9321,
+          "longitude": 18.8602
+        },
+        {
+          "name": "Centre Africain de Recherches sur Bananiers et Plantains (CARBAP)",
+          "city": "Douala",
+          "country": "Cameroon",
+          "latitude": 4.051056,
+          "longitude": 9.767869
+        },
+        {
+          "name": "University of Zululand",
+          "city": "Empangeni",
+          "country": "South Africa",
+          "latitude": -28.768229,
+          "longitude": 32.053578
+        },
+        {
+          "name": "Nelson Mandela Metropolitan University",
+          "city": "Port Elizabeth",
+          "country": "South Africa",
+          "latitude": -33.917,
+          "longitude": 25.5701
+        },
+        {
+          "name": "University of Nigeria Nsukka",
+          "city": "Nsukka",
+          "country": "Nigeria",
+          "latitude": 6.8614,
+          "longitude": 7.4081
+        },
+        {
+          "name": "University of South Africa (UNISA)",
+          "city": "Pretoria",
+          "country": "South Africa",
+          "latitude": -25.7449,
+          "longitude": 28.1878
+        },
+        {
+          "name": "Makerere University",
+          "city": "Kampala",
+          "country": "Uganda",
+          "latitude": 0.3365,
+          "longitude": 32.5825
+        },
+        {
+          "name": "Cape Penisula University of Technology",
+          "city": "Cape Town",
+          "country": "South Africa",
+          "latitude": -33.9249,
+          "longitude": 18.4241
+        },
+        {
+          "name": "i-Themba Labs",
+          "city": "Somerset West",
+          "country": "South Africa",
+          "latitude": -34.0762,
+          "longitude": 18.8433
+        },
+        {
+          "name": "International Centre for Genetic Engineering and Biotechnology (ICGEB)",
+          "city": "Cape Town",
+          "country": "South Africa",
+          "latitude": -33.9249,
+          "longitude": 18.4241
+        },
+        {
+          "name": "University of Ouagadougou",
+          "city": "Ouagadougou",
+          "country": "Burkina Faso",
+          "latitude": 12.3714,
+          "longitude": -1.5197
+        },
+        {
+          "name": "International Center for Tropical Agriculture (TSBF-CIAT)",
+          "city": "Nairobi",
+          "country": "Kenya",
+          "latitude": -1.286389,
+          "longitude": 36.817223
+        },
+        {
+          "name": "Stellenbosch University",
+          "city": "Stellenbosch",
+          "country": "South Africa",
+          "latitude": -33.9321,
+          "longitude": 18.8602
+        },
+        {
+          "name": "University of Kwazulu-Natal",
+          "city": "Durban",
+          "country": "South Africa",
+          "latitude": -29.8587,
+          "longitude": 31.0218
+        },
+        {
+          "name": "University of Dar es Salaam",
+          "city": "Dar es Salaam",
+          "country": "Tanzania",
+          "latitude": -6.7924,
+          "longitude": 39.2083
+        },
+        {
+          "name": "National Institute for Communicable Diseases (NICD)",
+          "city": "Johannesburg",
+          "country": "South Africa",
+          "latitude": -26.2041,
+          "longitude": 28.0473
+        }
+
+      ]
     },
     "sudamerica": {
       "members": {
@@ -323,6 +641,9 @@ export default new Vuex.Store({
     getAlumniSudAmerica: state => {
       return state.africa.alumni
     },
+    getInstitutes: state => {
+      return state.africa.institutes
+    }
   },
   mutations: {
   },
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,7 +4,13 @@
     <div class="map" ref="map"></div>
 
     <div class="selected-country">
-      <h2 :style="'color: rgb('+this.mapR+','+this.mapG+','+this.mapB+')'">{{ this.selectedCountry }}</h2>
+      <h2
+        :style="
+          'color: rgb(' + this.mapR + ',' + this.mapG + ',' + this.mapB + ')'
+        "
+      >
+        {{ this.selectedCountry }}
+      </h2>
     </div>
 
     <div class="button-filters-1">
@@ -12,13 +18,11 @@
       <button @click="changeLayer(members)">Members</button>
       <button @click="changeLayer(alumni)">Alumni</button>
     </div>
-    
+
     <div class="button-filters-3">
       <button class="btn-africa" @click="goToAfrica()">Africa</button>
       <button class="btn-samerica" @click="goToSAmerica()">Sud America</button>
     </div>
-    
-
   </div>
 </template>
 
@@ -40,14 +44,14 @@ export default {
       baseMapColor: "#b0b0b0",
       hoverMapColor: "#206020",
       hoverLineColor: "#ecd041",
-      selectedCountry: '',
+      selectedCountry: "",
       mapR: 45,
       mapG: 134,
       mapB: 45,
       /* tooltip_top: 0,
       tooltip_left: 0,
       hoverMembers: 0, */
-      displayHover: false
+      displayHover: false,
     };
   },
   methods: {
@@ -55,34 +59,34 @@ export default {
       this.countryKeys = Object.entries(this.mapData);
     },
     changeLayer(data) {
-      this.mapData = data
+      this.mapData = data;
       if (this.map) {
-        this.map.removeLayer('countries-data')
-        this.map.removeLayer('countries-titles')
-        this.colorCountries()
+        this.map.removeLayer("countries-data");
+        this.map.removeLayer("countries-titles");
+        this.colorCountries();
       }
     },
     goToAfrica() {
       this.map.flyTo({
         center: [20, 0],
         zoom: 3,
-      })
-      this.mapR = 45
-      this.mapG = 134
-      this.mapB = 45
-      this.zoomedto = 'Africa'
-      this.changeLayer(this.members)
+      });
+      this.mapR = 45;
+      this.mapG = 134;
+      this.mapB = 45;
+      this.zoomedto = "Africa";
+      this.changeLayer(this.members);
     },
     goToSAmerica() {
       this.map.flyTo({
         center: [-70, -30],
         zoom: 2.9,
-      })
-      this.mapR = 0
-      this.mapG = 153
-      this.mapB = 204
-      this.zoomedto = 'SudAmerica'
-      this.changeLayer(this.members)
+      });
+      this.mapR = 0;
+      this.mapG = 153;
+      this.mapB = 204;
+      this.zoomedto = "SudAmerica";
+      this.changeLayer(this.members);
     },
     buildMap() {
       var vm = this;
@@ -110,6 +114,14 @@ export default {
           type: "vector",
           url: "mapbox://mapbox.mapbox-streets-v8",
         });
+
+        this.map.loadImage(
+          "institution.png",
+          (error, image) => {
+            if (error) throw error;
+            this.map.addImage("custom-marker", image);
+          }
+        );
 
         this.map.addLayer(
           {
@@ -164,10 +176,18 @@ export default {
           "country-labels"
         );
 
+        this.map.addSource("points", {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: this.institutes,
+          },
+        });
+
         this.colorCountries();
       });
 
-      vm = this
+      vm = this;
 
       this.map.on("click", function (e) {
         var point = vm.map.queryRenderedFeatures(e.point, {
@@ -177,11 +197,10 @@ export default {
         //if it finds a country under hit point
         if (point) {
           console.log(point.properties.NAME_EN, point.properties.ISO_A3);
-          vm.selectedCountry = point.properties.NAME_EN
+          vm.selectedCountry = point.properties.NAME_EN;
         } else {
-          console.log('empty click');
+          console.log("empty click");
         }
-
       });
 
       let hoveredStateId = null;
@@ -190,7 +209,7 @@ export default {
           layers: ["state-fills"],
         });
 
-        //eset hover state 
+        //eset hover state
         if (hoveredStateId !== null) {
           vm.map.setFeatureState(
             {
@@ -256,13 +275,13 @@ export default {
       // Build a GL match expression that defines the color for every vector tile feature
       // Use the ISO 3166-1 alpha 3 code as the lookup key for the country shape
       const matchExpression = ["match", ["get", "NAME_EN"]]; //optimally use iso_3166_1_alpha_3
-      const matchExpressionTitle = ['match', ['get', 'name_en']];
+      const matchExpressionTitle = ["match", ["get", "name_en"]];
 
       // Calculate color values for each country based on 'hdi' value
       var max = 0;
       var keys = [];
       //if (this.selectedWG == 0)
-        keys = Object.entries(this.mapData);
+      keys = Object.entries(this.mapData);
       //else keys = Object.entries(this.mapData.workgroup.country);
 
       for (const row of keys) if (row[1] > max) max = row[1];
@@ -271,11 +290,10 @@ export default {
         // Convert the range of data values to a suitable color
         const alpha = row[1] / max + 0.2;
         let color = `rgba(${this.mapR}, ${this.mapG}, ${this.mapB}, ${alpha})`;
-        if(row[1] == 0)
-          color = '#e3e3e3'
-        
+        if (row[1] == 0) color = "#e3e3e3";
+
         // Convert the range of data values to the country name or to the number for the value
-        const title = row[1] > 0 ? row[1] : '';
+        const title = row[1] > 0 ? row[1] : "";
         let titlec = `${title}`;
 
         matchExpression.push(row[0], color);
@@ -284,7 +302,7 @@ export default {
 
       // Last value is the default, used where there is no data
       matchExpression.push(this.baseMapColor);
-      matchExpressionTitle.push('')
+      matchExpressionTitle.push("");
 
       // Add layer from the vector tile source to create the choropleth
       // Insert it below the 'admin-1-boundary-bg' layer in the style
@@ -296,7 +314,7 @@ export default {
           "source-layer": "ne_10m_admin_0_countries-45dujq",
           paint: {
             "fill-color": matchExpression,
-          }
+          },
         },
         "state-stroke"
       );
@@ -312,116 +330,69 @@ export default {
           "text-anchor": "top",
         },
         paint: {
-          "text-color": "#fff"
-        }
+          "text-color": "#fff",
+        },
       });
-      
+
+      this.map.addLayer(
+        {
+          id: "points",
+          type: "symbol",
+          source: "points",
+          layout: {
+            'text-field': ['get', 'title'],
+            'text-font': ['Open Sans Bold'],
+            'text-size': 12,
+            'text-allow-overlap': true,
+            "icon-image": "custom-marker",
+            'text-allow-overlap': true,
+            'text-ignore-placement': true,
+            'icon-allow-overlap': true,
+            'icon-ignore-placement': true
+          },
+          minzoom: 2,
+        },
+        "countries-titles"
+      );
     },
   },
   computed: {
     members() {
-      if(this.zoomedto == 'Africa')
-        return this.$store.getters.getAfrica
-      else 
-        return this.$store.getters.getSudAmerica
-
+      if (this.zoomedto == "Africa") return this.$store.getters.getAfrica;
+      else return this.$store.getters.getSudAmerica;
     },
     phds() {
-      if(this.zoomedto == 'Africa')
-        return this.$store.getters.getphds
-      else  
-        return this.$store.getters.getPhdSudAmerica
+      if (this.zoomedto == "Africa") return this.$store.getters.getphds;
+      else return this.$store.getters.getPhdSudAmerica;
     },
     alumni() {
-      if(this.zoomedto == 'Africa')
-        return this.$store.getters.getAlumniAfrica
-      else  
-        return this.$store.getters.getAlumniSudAmerica
-    }
+      if (this.zoomedto == "Africa") return this.$store.getters.getAlumniAfrica;
+      else return this.$store.getters.getAlumniSudAmerica;
+    },
+    institutes() {
+      let ints = this.$store.getters.getInstitutes;
+      var features = [];
+
+      ints.forEach((el) => {
+        features.push({
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [el.longitude, el.latitude],
+          },
+          properties: {
+            title: el.name,
+          },
+        });
+      });
+
+      return features;
+    },
   },
   async mounted() {
-    this.mapData = {
-      tot: {
-        members: "427",
-        programs: "90",
-        materials: "78",
-        evaluations: "3",
-        projects: "11",
-      },
-      all_workgroups: {
-        country: {
-          ITA: 54,
-          DNK: 16,
-          DEU: 76,
-          ESP: 35,
-          CHE: 7,
-          NLD: 22,
-          SVN: 4,
-          ISR: 5,
-          GBR: 9,
-          IRL: 6,
-          LUX: 2,
-          NOR: 1,
-          POL: 3,
-          MLT: 1,
-          CZE: 10,
-          FRA: 18,
-          FIN: 6,
-          BEL: 6,
-          SVK: 2,
-          CYP: 1,
-          HUN: 9,
-          ROU: 9,
-          EGY: 3,
-          GRC: 9,
-          PRT: 5,
-          AUT: 8,
-          LVA: 3,
-          BGR: 2,
-          TUR: 11,
-          IND: 5,
-          AUS: 1,
-          USA: 10,
-          HRV: 1,
-          SWE: 3,
-          UKR: 7,
-          RUS: 1,
-          BGD: 1,
-          HKG: 1,
-          THA: 1,
-          CHN: 3,
-          JPN: 2,
-          CAN: 3,
-          IRN: 1,
-          EST: 1,
-          PAK: 1,
-          MKD: 1,
-          CMR: 1,
-          country_non_settato: 40,
-        },
-        members: {
-          tot: 427,
-          experience_1: 55,
-          experience_2: 30,
-          experience_3: 74,
-          experience_non_settato: 268,
-        },
-        members_roles: {
-          Consultant: 38,
-          Educator: 97,
-          "Event organizer": 44,
-          Researcher: 110,
-          "Science communicator": 47,
-          "Tool builder": 30,
-          "Other (specify)": 3,
-          role_non_settato: 273,
-        },
-      },
-    };
-    
-    this.mapData = this.members
+    this.mapData = this.members;
 
-    this.fetchMapData(); 
+    this.fetchMapData();
 
     await this.buildMap();
   },
@@ -445,9 +416,8 @@ export default {
   z-index: 10;
   bottom: 3rem;
   right: 4rem;
-  gap: .5rem;
+  gap: 0.5rem;
 }
-
 
 .button-filters-3 {
   position: fixed;
@@ -456,7 +426,7 @@ export default {
   z-index: 10;
   bottom: 3rem;
   left: 4rem;
-  gap: .5rem;
+  gap: 0.5rem;
 }
 
 .selected-country {
